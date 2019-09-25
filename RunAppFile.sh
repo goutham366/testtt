9336c01a1b205cae54ca217846617d3e5903e5b9
Uday Kumar Nuthi <uday.nuthi@ad.infosys.com>
2019-09-25 15:22:53 +0530

    test

--- RunAppFile.sh
+++ RunAppFile.sh
@@ -3,4 +3,4 @@ cd '/home/ec2-user/AngularProject-WBUI'
 npm install
 npm install -g @angular/cli
 ng build --prod
-cp -rf /home/ec2-user/AngularProject-WBUI/opt/tomcat/webapps
+cp -rf /home/ec2-user/AngularProject-WBUI /opt/tomcat/webapps
