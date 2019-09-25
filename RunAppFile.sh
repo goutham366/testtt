ec70e07b1ce54ecf06ba32d29507d70ab7ad8715
Uday Kumar Nuthi <uday.nuthi@ad.infosys.com>
2019-09-25 18:09:42 +0530

    test

--- RunAppFile.sh
+++ RunAppFile.sh
@@ -1,6 +1,6 @@
 echo 'Starting my app' 
-cd '/home/ec2-user/warnerbrosAWS-ui'
+cd '/home/ec2-user/warnerbros-ui'
 npm install
 npm install -g @angular/cli
 npm run ng build
-sudo cp -rf /home/ec2-user/warnerbrosAWS-ui/dist /opt/tomcat/webapps
+sudo cp -rf /home/ec2-user/warnerbros-ui/dist /opt/tomcat/webapps
