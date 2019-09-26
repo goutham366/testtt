311dc2d53f221924fd0bfb7fdb0310da742730f1
Uday Kumar Nuthi <uday.nuthi@ad.infosys.com>
2019-09-26 15:12:59 +0530

    test

--- RunAppFile.sh
+++ RunAppFile.sh
@@ -3,4 +3,5 @@ cd '/home/ec2-user/warnerbros-ui'
 npm install
 npm install -g @angular/cli
 npm run ng build
+sed -i 's/href="/"/href="."/g' /home/ec2-user/warnerbros-ui/dist/warnerbros-ui/index.html
 sudo cp -rf /home/ec2-user/warnerbros-ui/dist/warnerbros-ui /opt/tomcat/webapps
