edf06bc74d72ab1cd5d698c6913374c63d0a595a
Anusha Sanaboyina <anusha.sanaboyina@ad.infosys.com>
2019-11-15 16:30:53 +0530

    added s3 url

--- RunAppFile.sh
+++ RunAppFile.sh
@@ -1,7 +1,7 @@
 echo 'Starting my app' 
-cd '/home/ec2-user/warnerbros-ui'
+cd '/home/ec2-user/warnerbrosUI'
 npm install
 npm install -g @angular/cli
 rm -rf dist
 npm run ng build --prod
-sudo cp -rf /home/ec2-user/warnerbros-ui/dist/warnerbros-ui /opt/tomcat/webapps
+sudo cp -rf /home/ec2-user/warnerbrosUI/dist/warnerbros-ui /opt/tomcat/webapps
